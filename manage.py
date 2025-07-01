3226b78b38c14eb3f2643aa8806384bac6d22dcf
wanghui <1131141916@qq.com>
2025-07-01 10:15:14 +0800

    dev

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
+    """Run administrative tasks.1111"""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "application.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +18,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
